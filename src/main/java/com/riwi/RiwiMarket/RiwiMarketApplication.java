3db7ea192e9fa032abbca3fcf4773be0a6586365
Cristian <tiancarvelasquez@gmail.com>
2024-06-25 10:32:59 -0500

    first commit

---
+++ src/main/java/com/riwi/RiwiMarket/RiwiMarketApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.RiwiMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwiMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/RiwiMarket/RiwiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.RiwiMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
