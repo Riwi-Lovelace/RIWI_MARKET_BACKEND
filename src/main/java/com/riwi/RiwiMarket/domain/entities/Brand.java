20a03a52f7fb954d5bc45acdf52bd7640d22287e
Cristian <tiancarvelasquez@gmail.com>
2024-06-25 11:02:55 -0500

    Create entity brand

---
+++ src/main/java/com/riwi/RiwiMarket/domain/entities/Brand.java
@@ -0,0 +1,27 @@
+package com.riwi.RiwiMarket.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Brand {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Boolean status;
+}
