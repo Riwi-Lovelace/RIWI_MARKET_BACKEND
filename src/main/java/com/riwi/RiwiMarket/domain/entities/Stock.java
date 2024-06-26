7a95f087af24036e9ed23858cefb936288c1c71a
Darwing <Darwingx990@gmail.com>
2024-06-25 22:17:48 -0500

    EntityStock created,

---
+++ src/main/java/com/riwi/RiwiMarket/domain/entities/Stock.java
@@ -0,0 +1,29 @@
+package com.riwi.RiwiMarket.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    @Column(columnDefinition = "DECIMAL(5,2)")
+    private BigDecimal weight;
+    @Column(columnDefinition = "DECIMAL(10,2)", nullable = false)
+    private BigDecimal purchasePrice;
+    @Column(nullable = false)
+    private int dateId;
+    @Column(length = 100)
+    private String supplierId;
+}
