0452d835e8ce8a69801132f4b8695985b814a32b
Mateo <mateosvidales24@gmail.com>
2024-06-26 18:43:36 -0500

    Payroll entity relations

--- src/main/java/com/riwi/RiwiMarket/domain/entities/Payroll.java
+++ src/main/java/com/riwi/RiwiMarket/domain/entities/Payroll.java
@@ -3,9 +3,12 @@ package com.riwi.RiwiMarket.domain.entities;
 import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,4 +30,7 @@ public class Payroll {
     @Column(nullable = false)
     private LocalDateTime end;
     
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    private Employee employeeId;
 }
