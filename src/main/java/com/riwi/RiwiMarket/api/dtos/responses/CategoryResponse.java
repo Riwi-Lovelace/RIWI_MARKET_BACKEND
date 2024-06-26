7685519c623d6e7212af1935fbfcca4c607e310c
Kristiamnx <rivaskristopher24@gmail.com>
2024-06-26 16:53:28 -0500

    Creation of entity base (DTOs - Repository -Iservice - Service - Controller)

---
+++ src/main/java/com/riwi/RiwiMarket/api/dtos/responses/CategoryResponse.java
@@ -0,0 +1,15 @@
+package com.riwi.RiwiMarket.api.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryResponse {
+    private String name;
+    private boolean status;
+}
