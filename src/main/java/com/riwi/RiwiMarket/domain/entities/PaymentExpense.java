fb04e0f820d4b1d512a6cce0aef449ee921c431e
Mateo <mateosvidales24@gmail.com>
2024-06-26 19:14:18 -0500

    payment expense entity relations

--- src/main/java/com/riwi/RiwiMarket/domain/entities/PaymentExpense.java
+++ src/main/java/com/riwi/RiwiMarket/domain/entities/PaymentExpense.java
@@ -1,9 +1,12 @@
 package com.riwi.RiwiMarket.domain.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,4 +21,14 @@ public class PaymentExpense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "expense_id", referencedColumnName = "id")
+    private Expense expenseId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pocket_id", referencedColumnName = "id")
+    private Pocket pocketId;
+
+
 }
