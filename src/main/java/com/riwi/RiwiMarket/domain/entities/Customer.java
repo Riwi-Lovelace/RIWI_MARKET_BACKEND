1e4fb7e6c49d97599bfc5d2e057a0062d1a8b537
Alejandro Estrada <estradaprograma.23.01@gmail.com>
2024-06-27 17:07:59 -0500

    reletionship between entities

--- src/main/java/com/riwi/RiwiMarket/domain/entities/Customer.java
+++ src/main/java/com/riwi/RiwiMarket/domain/entities/Customer.java
@@ -1,14 +1,21 @@
 package com.riwi.RiwiMarket.domain.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity(name = "customer")
 @Data
@@ -32,5 +39,15 @@ public class Customer {
     private String password;
     @Column(length = 20)
     private String document;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            mappedBy = "customer",
+            cascade = CascadeType.ALL,
+            orphanRemoval = false
+    )
+    private List<Customer> customers;
 
 }
