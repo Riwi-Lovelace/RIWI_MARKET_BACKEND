8071981198a47b9e5a879ed0cfcf016827b613ab
Alejandro Estrada <estradaprograma.23.01@gmail.com>
2024-06-27 17:32:11 -0500

    relationship between entities

--- src/main/java/com/riwi/RiwiMarket/domain/entities/SalePayment.java
+++ src/main/java/com/riwi/RiwiMarket/domain/entities/SalePayment.java
@@ -1,9 +1,12 @@
 package com.riwi.RiwiMarket.domain.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,4 +21,13 @@ public class SalePayment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sale_payment_id",referencedColumnName = "id",nullable = false)
+    private SalePayment salePayment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payment_id",referencedColumnName = "id",nullable = false)
+    private Payment Payment;
+
 }
