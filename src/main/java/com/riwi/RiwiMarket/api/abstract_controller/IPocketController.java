f0c1fd75f4a42c7863e5ab3b964ee055d8526b8a
Santiago Echevery Serna <santiagocherrys@hotmail.com>
2024-06-29 11:55:34 -0500

    Change IPocketController to abstract controller and Added @Component to SupportService in order to function cause in the develop update is missing this parameter

--- src/main/java/com/riwi/RiwiMarket/api/abstract_controller/IPocketController.java
+++ src/main/java/com/riwi/RiwiMarket/api/abstract_controller/IPocketController.java
@@ -1,5 +1,6 @@
-package com.riwi.RiwiMarket.api.controllers;
+package com.riwi.RiwiMarket.api.abstract_controller;
 
+import com.riwi.RiwiMarket.api.abstract_controller.GenericController;
 import com.riwi.RiwiMarket.api.dtos.requests.PocketRequest;
 import com.riwi.RiwiMarket.api.dtos.responses.PocketResponse;
 import org.apache.coyote.Response;
@@ -7,6 +8,6 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
-public interface IPocketController extends GenericController<PocketRequest, PocketResponse,Long>{
+public interface IPocketController extends GenericController<PocketRequest, PocketResponse,Long> {
     ResponseEntity<List<PocketResponse>> getAll();
 }
