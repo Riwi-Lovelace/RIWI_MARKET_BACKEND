b451800c40f76cc2c1dff12637e492d10093f11b
Santiago Echevery Serna <santiagocherrys@hotmail.com>
2024-06-30 10:47:49 -0500

    US-007-002/Edit-Pocket/Fix: bring the update of develop, change DTOs with getter and setters and add @Component to SupportService

--- src/main/java/com/riwi/RiwiMarket/api/abstract_controller/GenericController.java
+++ src/main/java/com/riwi/RiwiMarket/api/abstract_controller/GenericController.java
@@ -1,6 +1,10 @@
 package com.riwi.RiwiMarket.api.abstract_controller;
 
+import com.riwi.RiwiMarket.api.dtos.requests.PocketRequest;
+import com.riwi.RiwiMarket.api.dtos.responses.PocketResponse;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface GenericController<RequestDTO, ResponseDTO, ID> {
 
@@ -12,4 +16,7 @@ public interface GenericController<RequestDTO, ResponseDTO, ID> {
 
     ResponseEntity<Void> delete(ID id);
 
+    interface IPocketController extends GenericController<PocketRequest, PocketResponse,Long>{
+        ResponseEntity<List<PocketResponse>> getAll();
+    }
 }
--- src/main/java/com/riwi/RiwiMarket/api/controllers/PocketController.java
+++ src/main/java/com/riwi/RiwiMarket/api/controllers/PocketController.java
@@ -1,5 +1,6 @@
 package com.riwi.RiwiMarket.api.controllers;
 
+import com.riwi.RiwiMarket.api.abstract_controller.GenericController;
 import com.riwi.RiwiMarket.api.dtos.requests.PocketRequest;
 import com.riwi.RiwiMarket.api.dtos.responses.PocketResponse;
 import com.riwi.RiwiMarket.infrastructure.abstract_services.IPocketService;
@@ -12,7 +13,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +22,7 @@ import java.util.List;
 @RequestMapping(path = "/pockets")
 @AllArgsConstructor
 @Tag(name= "Pockets")
-public class PocketController implements IPocketController {
+public class PocketController implements GenericController.IPocketController {
 
     @Autowired
     private final IPocketService pocketService;
