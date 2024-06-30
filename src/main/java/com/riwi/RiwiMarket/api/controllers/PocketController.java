ed5f6cca499778937b99fcd1a5cbef1b19602e2d
Santiago Echevery Serna <santiagocherrys@hotmail.com>
2024-06-30 10:58:47 -0500

    US-007-004/Get-All-Pocket/Fix: bring the update of develop, change DTOs with getter and setters and add @Component to SupportService

--- src/main/java/com/riwi/RiwiMarket/api/controllers/PocketController.java
+++ src/main/java/com/riwi/RiwiMarket/api/controllers/PocketController.java
@@ -1,5 +1,6 @@
 package com.riwi.RiwiMarket.api.controllers;
 
+import com.riwi.RiwiMarket.api.abstract_controller.IPocketController;
 import com.riwi.RiwiMarket.api.dtos.requests.PocketRequest;
 import com.riwi.RiwiMarket.api.dtos.responses.PocketResponse;
 import com.riwi.RiwiMarket.infrastructure.abstract_services.IPocketService;
@@ -12,7 +13,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
