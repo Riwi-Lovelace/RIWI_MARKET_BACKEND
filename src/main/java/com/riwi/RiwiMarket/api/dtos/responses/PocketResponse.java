14f63133a2728e8ff8347fe35db5e3e36e133e45
Santiago Echevery Serna <santiagocherrys@hotmail.com>
2024-07-01 11:41:30 -0500

    EP-007-Money-Pocket-System/Refactor Added id to the response that it was missing

--- src/main/java/com/riwi/RiwiMarket/api/dtos/responses/PocketResponse.java
+++ src/main/java/com/riwi/RiwiMarket/api/dtos/responses/PocketResponse.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PocketResponse {
+    private Long id;
     private BigDecimal amount;
     private TypePocket type;
     private String description;
