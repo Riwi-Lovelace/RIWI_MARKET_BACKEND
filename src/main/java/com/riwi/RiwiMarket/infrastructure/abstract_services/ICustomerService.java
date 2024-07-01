55631a4f29e2704cf9cdf5a2e1e5da92461fcda0
caritogcdev <carito940905@gmail.com>
2024-06-30 22:50:50 -0500

    US-005-002/Creation-of-End-User-Defaults/Feat: Add the createUserDefault method in the customer iterface, service and controller

--- src/main/java/com/riwi/RiwiMarket/infrastructure/abstract_services/ICustomerService.java
+++ src/main/java/com/riwi/RiwiMarket/infrastructure/abstract_services/ICustomerService.java
@@ -9,4 +9,5 @@ import com.riwi.RiwiMarket.util.enums.SortCustomer;
 public interface ICustomerService extends GenericService<CustomerRequest, CustomerResponse, Long>{
     public Page<CustomerResponse> getAll(int size, int page, SortCustomer sortCustomer);
     public final String FIELD_SORT = "name";
+    String createUserDefault (String userDefault);
 }
