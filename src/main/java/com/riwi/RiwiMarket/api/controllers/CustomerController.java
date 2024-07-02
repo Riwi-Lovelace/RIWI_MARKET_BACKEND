bceea10c723b88603c55554e9e94d89ea335671a
JuanRendon1206 <juanjoseflorezrendon124@gmail.com>
2024-07-01 23:50:12 -0500

    EP-005/CUSTOMER-Management-System-Complete/Refactor/: Update-with-ultimate-user-history-and-delete-DataBase-in-application-properties

--- src/main/java/com/riwi/RiwiMarket/api/controllers/CustomerController.java
+++ src/main/java/com/riwi/RiwiMarket/api/controllers/CustomerController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.riwi.RiwiMarket.api.abstract_controller.GenericController;
 import com.riwi.RiwiMarket.api.dtos.requests.CustomerRequest;
 import com.riwi.RiwiMarket.api.dtos.responses.CustomerResponse;
 import com.riwi.RiwiMarket.infrastructure.abstract_services.ICustomerService;
