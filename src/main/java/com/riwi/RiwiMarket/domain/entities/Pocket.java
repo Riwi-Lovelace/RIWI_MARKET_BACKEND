853b9fc20cb7c38106013a332854b6514f7afa3f
Cristian <tiancarvelasquez@gmail.com>
2024-07-02 07:30:52 -0500

    create relationship of Pocket entity

--- src/main/java/com/riwi/RiwiMarket/domain/entities/Pocket.java
+++ src/main/java/com/riwi/RiwiMarket/domain/entities/Pocket.java
@@ -1,12 +1,18 @@
 package com.riwi.RiwiMarket.domain.entities;
 
 import java.math.BigDecimal;
+import java.util.List;
+
 import com.riwi.RiwiMarket.util.enums.TypePocket;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,4 +34,10 @@ public class Pocket {
     private TypePocket type;
     @Column(columnDefinition = "TEXT")
     private String description;
+
+    @OneToMany(fetch = FetchType.EAGER,
+     mappedBy = "pocketId",
+     cascade = CascadeType.ALL,
+     orphanRemoval = false)
+    private List<PaymentExpense> paymentExpenses;
 }
