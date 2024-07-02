2461e00dc85cd7a4ff845d898fef06b445a1ad6d
Antony Martinez Ruiz <antonymaruiz02@gmail.com>
2024-07-02 10:40:13 -0500

    feature/US-006-002/bUG

--- src/main/java/com/riwi/RiwiMarket/infrastructure/services/SupplierService.java
+++ src/main/java/com/riwi/RiwiMarket/infrastructure/services/SupplierService.java
@@ -16,7 +16,6 @@ import com.riwi.RiwiMarket.util.exceptions.BadRequestException;
 
 import lombok.AllArgsConstructor;
 
-
 @Service
 @AllArgsConstructor
 public class SupplierService implements ISupplierService{
