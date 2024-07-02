42cf85b802640101ad8ec95a308c0910d6885e91
Kevin Mejia <kwmejia9@gmail.com>
2024-07-02 10:04:09 -0500

    develop/fix: remove anotation primaty in repositories

--- src/main/java/com/riwi/RiwiMarket/domain/repositories/CategoryRepository.java
+++ src/main/java/com/riwi/RiwiMarket/domain/repositories/CategoryRepository.java
@@ -2,11 +2,9 @@ package com.riwi.RiwiMarket.domain.repositories;
 
 import com.riwi.RiwiMarket.domain.entities.Category;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Primary
 @Repository
 public interface CategoryRepository  extends JpaRepository<Category, Long> {
 }
--- src/main/java/com/riwi/RiwiMarket/domain/repositories/SubcategoryRepository.java
+++ src/main/java/com/riwi/RiwiMarket/domain/repositories/SubcategoryRepository.java
@@ -1,11 +1,9 @@
 package com.riwi.RiwiMarket.domain.repositories;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.riwi.RiwiMarket.domain.entities.Subcategory;
-@Primary
 @Repository
 public interface SubcategoryRepository extends JpaRepository<Subcategory, Long> {
     
