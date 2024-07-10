822a261263dedc7ba2c5634c5300aabc7a3acd7d
NewNobbie <florezs043@gmail.com>
2024-07-10 10:44:57 -0500

    Feat: Preparation for the Epic

---
+++ src/main/java/com/riwi/RiwiMarket/domain/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.riwi.RiwiMarket.domain.repositories;
+
+import com.riwi.RiwiMarket.domain.entities.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
