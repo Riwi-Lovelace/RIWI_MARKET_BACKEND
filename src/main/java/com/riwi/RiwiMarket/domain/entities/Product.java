25db29dc154b149a28b8e93c414165f73d80a9ea
NewNobbie <florezs043@gmail.com>
2024-07-10 07:39:22 -0500

    fix: entity

--- src/main/java/com/riwi/RiwiMarket/domain/entities/Product.java
+++ src/main/java/com/riwi/RiwiMarket/domain/entities/Product.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 @Builder
 public class Product {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false, length = 13)
     private Long id;
