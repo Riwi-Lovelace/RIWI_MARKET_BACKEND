f608304d2c8eff5b3fcef6f8ef87df83087e1123
Santiago Echevery Serna <santiagocherrys@hotmail.com>
2024-07-10 06:46:33 -0500

    Ep-011/Management-System/Add service inyection/Feat: Add service object to storecontroller

--- src/main/java/com/riwi/RiwiMarket/api/controllers/StoreController.java
+++ src/main/java/com/riwi/RiwiMarket/api/controllers/StoreController.java
@@ -4,8 +4,10 @@ import com.riwi.RiwiMarket.api.abstract_controller.IStoreController;
 import com.riwi.RiwiMarket.api.dtos.requests.PocketRequest;
 import com.riwi.RiwiMarket.api.dtos.responses.PocketResponse;
 import com.riwi.RiwiMarket.api.dtos.responses.StoreResponse;
+import com.riwi.RiwiMarket.infrastructure.abstract_services.IStoreService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @Tag(name = "Store")
 public class StoreController implements IStoreController {
+
+    @Autowired
+    private final IStoreService storeService;
     @Override
     public ResponseEntity<PocketResponse> create(PocketRequest request) {
         return null;
