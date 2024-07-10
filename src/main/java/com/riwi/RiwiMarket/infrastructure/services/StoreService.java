ec76564865dc4c30ac78a11d58949bdfe1aba5c1
kemtch19 <kemtch19@gmail.com>
2024-07-10 12:25:17 -0500

    prueba de create

--- src/main/java/com/riwi/RiwiMarket/infrastructure/services/StoreService.java
+++ src/main/java/com/riwi/RiwiMarket/infrastructure/services/StoreService.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -31,7 +32,9 @@ public class StoreService implements IStoreService {
 
     @Override
     public StoreResponse create(StoreRequest request) {
-        return null;
+        Store store = this.storeMapper.toEntity(request);
+        store.setAvailable(new BigDecimal(0.0));
+        return this.storeMapper.toResponse(this.storeRepository.save(store));
     }
 
     @Override
