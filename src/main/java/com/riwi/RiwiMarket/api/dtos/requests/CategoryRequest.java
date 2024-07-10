b1bb2eb155ad36f865101546a5d09f65716aa625
antony martinez <antonymaruiz02@gmail.com>
2024-07-10 12:07:58 -0500

    Develop/fix: Change @data for @Getter and @Setter

--- src/main/java/com/riwi/RiwiMarket/api/dtos/requests/CategoryRequest.java
+++ src/main/java/com/riwi/RiwiMarket/api/dtos/requests/CategoryRequest.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
