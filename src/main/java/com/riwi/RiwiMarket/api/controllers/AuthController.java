5f3f80a6a3155d52c8070a476e78a6ca6a3e8ae5
caritogcdev <carito940905@gmail.com>
2024-07-12 10:42:50 -0500

    EP-040/Update/Feat:Spring Security basic configuration

--- src/main/java/com/riwi/RiwiMarket/api/controllers/AuthController.java
+++ src/main/java/com/riwi/RiwiMarket/api/controllers/AuthController.java
@@ -2,7 +2,6 @@ package com.riwi.RiwiMarket.api.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     @PostMapping(path = "/auth/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
-        return "Welcome " + username + " to Riwi Market";
+    public String login() {
+        return "Welcome to Riwi Market from login";
     }
 
     @PostMapping(path = "/auth/register")
-    public String register(@RequestParam("username") String username, @RequestParam("password") String password) {
-        return "Welcome " + username + " to Riwi Market";
+    public String register() {
+        return "Welcome to Riwi Market from register";
     }
 }
--- src/main/java/com/riwi/RiwiMarket/configurations/SecurityConfig.java
+++ src/main/java/com/riwi/RiwiMarket/configurations/SecurityConfig.java
@@ -9,8 +9,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final String[] PUBLIC_RESOURCES = { "/services/public/get", "/auth/**" };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return null;
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authRequest -> authRequest
+                        .requestMatchers(PUBLIC_RESOURCES).permitAll()
+                        .anyRequest().authenticated()
+                ).build();
     }
 }
