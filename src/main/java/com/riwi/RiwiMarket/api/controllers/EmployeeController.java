d77fd9c5fa4dfbca66124d8f30bacf33af6b7da6
Darwing <Darwingx990@gmail.com>
2024-07-12 22:56:28 -0500

    US-011-008/Feat: Adding GetById in Employee's service and controller

--- src/main/java/com/riwi/RiwiMarket/api/controllers/EmployeeController.java
+++ src/main/java/com/riwi/RiwiMarket/api/controllers/EmployeeController.java
@@ -4,11 +4,14 @@ import com.riwi.RiwiMarket.api.abstract_controller.IEmployeeController;
 import com.riwi.RiwiMarket.api.dtos.requests.EmployeeRequest;
 import com.riwi.RiwiMarket.api.dtos.responses.EmployeeResponse;
 import com.riwi.RiwiMarket.infrastructure.abstract_services.IEmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,8 +30,12 @@ public class EmployeeController implements IEmployeeController {
     }
 
     @Override
-    public ResponseEntity<EmployeeResponse> read(Long id) {
-        return null;
+    @GetMapping(path = "/{id}")
+    @Operation(
+            summary = "Get information of an employee.",
+            description = "In this section you will find info of an employee by giving his/her Id.")
+    public ResponseEntity<EmployeeResponse> read(@PathVariable Long id) {
+        return ResponseEntity.ok(employeeService.read(id));
     }
 
     @Override
