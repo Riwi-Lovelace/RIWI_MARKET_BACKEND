15777acded5317a0c2d7b966689ecd72c5bed153
MiguelAngelToro <rengell.angell@gmail.com>
2024-07-18 09:55:53 -0500

    US-015-001/Create-Stock/Feat: documentation for swagger

--- src/main/java/com/riwi/RiwiMarket/api/controllers/StockController.java
+++ src/main/java/com/riwi/RiwiMarket/api/controllers/StockController.java
@@ -6,11 +6,13 @@ import com.riwi.RiwiMarket.api.dtos.requests.StockRequest;
 import com.riwi.RiwiMarket.api.dtos.requests.StockWeightUpdateRequest;
 import com.riwi.RiwiMarket.api.dtos.responses.StockResponse;
 import com.riwi.RiwiMarket.infrastructure.abstract_services.IStockService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,8 @@ public class StockController implements IStockController {
     private final IStockService stockService;
 
     @Override
+    @Operation(summary = "Creat a pocket", description = "Creat a poket by sending the amount, the poket type CASH or BANK, and a description")
+    @PostMapping
     public ResponseEntity<StockResponse> create(@Validated @RequestBody StockRequest request) {
 
         return ResponseEntity.ok(this.stockService.create(request));
