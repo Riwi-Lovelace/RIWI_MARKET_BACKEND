32b8ca12e89dd01df0a767381c46d5bc77a59741
tejada <camilotejada1@gmail.com>
2024-07-18 19:40:06 -0500

    US-008-002/Create GetAll/refactor/feat-fix: Fixed logic errors, corrected fetch by date, added request refund get all to fetch information.

--- src/main/java/com/riwi/RiwiMarket/infrastructure/abstract_services/IRefundService.java
+++ src/main/java/com/riwi/RiwiMarket/infrastructure/abstract_services/IRefundService.java
@@ -1,6 +1,7 @@
 package com.riwi.RiwiMarket.infrastructure.abstract_services;
 
 import com.riwi.RiwiMarket.api.dtos.requests.RefundRequest;
+import com.riwi.RiwiMarket.api.dtos.requests.RefundRequestGetAll;
 import com.riwi.RiwiMarket.api.dtos.responses.RefundResponse;
 import com.riwi.RiwiMarket.util.enums.Method;
 import com.riwi.RiwiMarket.util.enums.Reason;
@@ -10,5 +11,6 @@ import java.time.LocalDate;
 
 public interface IRefundService extends GenericService<RefundRequest, RefundResponse, Long>{
 
-    Page<RefundResponse> getAll(int page, int size,Method method,Reason reason,LocalDate date,LocalDate dateEnd);
+    // Page<RefundResponse> getAll(int page, int size,Method method,Reason reason,LocalDate date,LocalDate dateEnd);
+    Page<RefundResponse> getAll1(RefundRequestGetAll request);
 }
