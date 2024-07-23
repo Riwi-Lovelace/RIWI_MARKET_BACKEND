1c4f18cd71472a2bacc3ee7b98a137a4563027f4
NewNobbie <florezs043@gmail.com>
2024-07-23 12:10:48 -0500

    Refactor: New attribute incomplete added to Status enum

--- src/main/java/com/riwi/RiwiMarket/domain/repositories/ExpenseRepository.java
+++ src/main/java/com/riwi/RiwiMarket/domain/repositories/ExpenseRepository.java
@@ -1,6 +1,9 @@
 package com.riwi.RiwiMarket.domain.repositories;
 
+import com.riwi.RiwiMarket.domain.entities.Employee;
 import com.riwi.RiwiMarket.domain.entities.Expense;
+import com.riwi.RiwiMarket.domain.entities.Supplier;
+import com.riwi.RiwiMarket.util.enums.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,5 +15,20 @@ import java.time.LocalDate;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
+    @Query("SELECT e FROM expense e WHERE" +
+            "(:id IS NULL OR e.id = :id) AND" +
+            "(:min IS NULL OR :max IS NULL OR e.amount BETWEEN :min AND :max)"+
+            "(:description IS NULL OR e.description LIKE %:description%) AND" +
+            "(:supplierId IS NULL OR e.supplierId = :supplierId) AND" +
+            "(:employeeId IS NULL OR e.employeeId = :employeeId) AND" +
+            "(:start IS NULL OR :end IS NULL OR e.date BETWEEN :start AND :end)")
 
+    Page<Expense>getAll(PageRequest request,
+                        @Param("id") Long id,
+                        @Param("min")BigDecimal min, @Param("max")BigDecimal max,
+                        @Param("description") String description,
+                        @Param("start")LocalDate start, @Param("end")LocalDate end,
+                        @Param("paidStatus") Status paidStatus,
+                        @Param("supplierId") Supplier supplierId,
+                        @Param("employeeId") Employee employeeId);
 }
--- src/main/java/com/riwi/RiwiMarket/infrastructure/abstract_services/IExpenseService.java
+++ src/main/java/com/riwi/RiwiMarket/infrastructure/abstract_services/IExpenseService.java
@@ -2,7 +2,10 @@ package com.riwi.RiwiMarket.infrastructure.abstract_services;
 
 import com.riwi.RiwiMarket.api.dtos.requests.ExpenseRequest;
 import com.riwi.RiwiMarket.api.dtos.responses.ExpenseCompleteResponse;
+import org.springframework.data.domain.Page;
 
 public interface IExpenseService extends GenericService<ExpenseRequest, ExpenseCompleteResponse, Long>{
+
+    Page<ExpenseCompleteResponse> getAll(ExpenseCompleteResponse request);
     
 }
